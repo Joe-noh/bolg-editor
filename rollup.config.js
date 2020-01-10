38da8b5b32251f4e66bfcda261d3efe0dd39fddc
Joe-noh <goflb.jh@gmail.com>
2020-01-10 23:32:01 +0900

    format rollup.comfig.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,12 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
-import pkg from './package.json';
+import svelte from 'rollup-plugin-svelte'
+import resolve from '@rollup/plugin-node-resolve'
+import pkg from './package.json'
 
 export default {
-	input: 'src/index.svelte',
-	output: [
-		{ file: pkg.module, 'format': 'es' },
-		{ file: pkg.main, 'format': 'umd', name: pkg.name }
-	],
-	plugins: [
-		svelte(),
-		resolve()
-	]
-};
+  input: 'src/index.svelte',
+  output: [
+    { file: pkg.module, format: 'es' },
+    { file: pkg.main, format: 'umd', name: pkg.name },
+  ],
+  plugins: [svelte(), resolve()],
+}
