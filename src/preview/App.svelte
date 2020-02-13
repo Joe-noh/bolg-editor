4cb5d77076431a17a4edeea8ab3bbd9232cbea49
Joe-noh <goflb.jh@gmail.com>
2020-02-14 01:57:50 +0900

    separate preview app into multiple pages

--- src/preview/App.svelte
+++ src/preview/App.svelte
@@ -1,27 +1,12 @@
 <script>
-  import { Container, Stack, Text } from '../../'
+  import Router from 'svelte-spa-router'
+  import Top from './routes/Top.svelte'
+  import Container from './routes/Container.svelte'
+
+  const routes = {
+    '/': Top,
+    '/container': Container,
+  }
 </script>
 
-<Stack gap="m">
-  <div class="bordered">
-    <Container padding="s">
-      <Text font="sans">Sans text in S padding Container</Text>
-    </Container>
-  </div>
-  <div class="bordered">
-    <Container padding="m">
-      <Text font="serif">Serif text in M padding Container</Text>
-    </Container>
-  </div>
-  <div class="bordered">
-    <Container padding="l">
-      <Text font="mono">Mono text in L padding Container</Text>
-    </Container>
-  </div>
-</Stack>
-
-<style>
-  .bordered {
-    border: solid 1px #222;
-  }
-</style>
+<Router {routes} />
---
+++ src/preview/routes/Container.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { Container, Stack, Text } from '../../'
+</script>
+
+<Stack gap="m">
+  <div class="bordered">
+    <Container padding="s">
+      <Text font="sans">Hello from S padding Container</Text>
+    </Container>
+  </div>
+  <div class="bordered">
+    <Container padding="m">
+      <Text font="sans">Hello from M padding Container</Text>
+    </Container>
+  </div>
+  <div class="bordered">
+    <Container padding="l">
+      <Text font="sans">Hello from L padding Container</Text>
+    </Container>
+  </div>
+</Stack>
+
+<style>
+  .bordered {
+    border: solid 1px #222;
+  }
+</style>
