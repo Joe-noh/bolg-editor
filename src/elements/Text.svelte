8d5b21531fd885a460374d5488a972815986ac54
Joe-noh <goflb.jh@gmail.com>
2020-02-16 22:32:46 +0900

    support text font-size

--- src/elements/Text.svelte
+++ src/elements/Text.svelte
@@ -1,9 +1,10 @@
 <script>
   export let font = 'sans'
   export let tone = 'normal'
+  export let size = 'm'
 </script>
 
-<span class="text {`font-${font}`} {`tone-${tone}`}">
+<span class="text {`font-${font}`} {`tone-${tone}`} {`size-${size}`}">
   <slot />
 </span>
 
@@ -28,6 +29,15 @@
     }
     &.tone-critical {
       color: $critical-color;
+    }
+    &.size-s {
+      font-size: $text-font-size-s;
+    }
+    &.size-m {
+      font-size: $text-font-size-m;
+    }
+    &.size-l {
+      font-size: $text-font-size-l;
     }
   }
 </style>
